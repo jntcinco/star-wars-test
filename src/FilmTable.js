1b8659c1474cd2f4c098015b66d20d805f1b0594
Jose-Noel Cinco <cincoj@ter.teradyne.com>
2020-01-09 10:04:34 +0800

    add files

---
+++ src/FilmTable.js
@@ -0,0 +1,60 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import React from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import Flag from 'react-flags';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; 
+import BootstrapTable from 'react-bootstrap-table-next';
+
+const API_URL = 'https://swapi.co/api/films/';
+
+class FilmTable extends React.Component {
+	state = {
+    films: [],
+    columns: [
+      {
+        dataField: 'episode_id',
+        text: 'Episode ID',
+        sort: true
+      },
+      {
+        dataField: 'title',
+        text: 'Film Title'
+      }
+    ]
+  }
+
+  componentDidMount() {
+    axios.get(API_URL).then(res => {
+      const films = res.data.results;
+      this.setState({ films });
+      console.debug("response: "+this.state.films);
+    })
+  }
+
+	render() {
+		return (
+			<div className="container">
+				<div className="col-xs-8">
+					<h1>Starwars Test</h1>
+					<div className="card">
+						<div className="card-body">
+							<h5 className="card-title">Films</h5>
+                <BootstrapTable 
+                 striped
+                 hover
+                 keyField='episode_id' 
+                 data={ this.state.films } 
+                 columns={ this.state.columns } />
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default FilmTable;
