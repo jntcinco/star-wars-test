6498cd0f56053bfde31bf7567f44b0139ffbbfad
Jose-Noel Cinco <jntcinco@gmail.com>
2020-01-09 20:04:28 +0800

    add favorites function

--- src/FilmTable.js
+++ src/FilmTable.js
@@ -10,51 +10,84 @@ import BootstrapTable from 'react-bootstrap-table-next';
 import filterFactory, { textFilter, Comparator } from 'react-bootstrap-table2-filter';
 import paginationFactory from 'react-bootstrap-table2-paginator';
 
+const ADD_TO_FAVORITE_URL = 'http://localhost:4000/favorites/add';
+const GET_FAVORITES_URL = 'http://localhost:4000/favorites/get_by/';
 const API_URL = 'https://swapi.co/api/films/';
 
 class FilmTable extends React.Component {
-	state = {
-    films: [],
-    columns: [
-      {
-        dataField: 'episode_id',
-        text: 'Episode ID',
-        sort: true
-      },
-      {
-        dataField: 'title',
-        text: 'Title',
-        filter: textFilter({
-          placeholder: 'Search by title...',  // custom the input placeholder
-          className: 'my-custom-text-filter', // custom classname on input
-          defaultValue: '', // default filtering value
-          comparator: Comparator.LIKE, // default is Comparator.LIKE
-          caseSensitive: true, // default is false, and true will only work when comparator is LIKE
-          style: { "width" : "200px", "float" : "left" }, // your custom styles on input
-        })
-      },
-      {
-        dataField: 'actions',
-        text: 'Actions'
-      }
-    ]
-  }
 
   constructor(props) {
     super(props);
+    this.state = {
+      isFavFilm: false,
+      films: [],
+      columns: [
+        {
+          dataField: 'episode_id',
+          text: 'Episode ID',
+          sort: true
+        },
+        {
+          dataField: 'title',
+          text: 'Title',
+          filter: textFilter({
+            placeholder: 'Search by title...',  // custom the input placeholder
+            className: 'my-custom-text-filter', // custom classname on input
+            defaultValue: '', // default filtering value
+            comparator: Comparator.LIKE, // default is Comparator.LIKE
+            caseSensitive: true, // default is false, and true will only work when comparator is LIKE
+            style: { "width" : "200px", "float" : "left" }, // your custom styles on input
+          })
+        },
+        {
+          dataField: 'actions',
+          text: 'Actions'
+        }
+      ]
+    }
+  }
+
+  addToFavorite(episode_id) {
+    var favorite = {"episode_id":episode_id.toString(), "is_favorite":true};
+    axios.post( ADD_TO_FAVORITE_URL, favorite).then(function(response) {
+      console.debug("naruto: "+response.data.message);
+      document.getElementById("span-"+episode_id).className = "glyphicon glyphicon-star";
+    })
+    .catch(function(error) {
+      console.error("naruto error: "+error);
+    });
+  }
+
+  async getStarwarsFilm() {
+    const favFilms = await axios.get(API_URL);
+    let films = await favFilms.data.results;
+    let finalFilms = [];
+    films.map((film, index) => {
+      let tempFilm = film;
+      var episode_id = film.episode_id;
+      const final_url = GET_FAVORITES_URL + episode_id;
+      let data = [];
+      axios.get(final_url).then(res => { 
+        data = res.data;
+        let length = data.length;
+        if(length > 0) {
+          tempFilm.actions = <button type="button" id={episode_id} className="btn btn-default btn-sm" onClick={this.addToFavorite.bind(this, episode_id)}>
+                          <span id={"span-"+episode_id} className="glyphicon glyphicon-star"></span> Add to Favourite
+                        </button>;
+        } else {
+          tempFilm.actions = <button type="button" id={episode_id} className="btn btn-default btn-sm" onClick={this.addToFavorite.bind(this, episode_id)}>
+                          <span id={"span-"+episode_id} className="glyphicon glyphicon-star-empty"></span> Add to Favourite
+                        </button>;
+        }
+        finalFilms.push(tempFilm);
+        this.setState({ films: finalFilms });
+      });
+    });
   }
 
   componentDidMount() {
-    axios.get(API_URL).then(res => {
-      const films = res.data.results;
-      films.map((film, index) => {
-        film.actions = <button type="button" className="btn btn-default btn-sm">
-                          <span className="glyphicon glyphicon-star-empty"></span> Add to Favourite
-                        </button>;
-      })
-      this.setState({ films });
-      console.debug("response: "+JSON.stringify(this.state.films));
-    })
+    this.getStarwarsFilm();
+    console.debug("film.actions: "+JSON.stringify(this.state.films));
   }
 
 	render() {
